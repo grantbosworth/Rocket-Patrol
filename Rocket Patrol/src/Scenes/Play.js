5c5af710333f2aa22fede20b3475625d923ada94
Grant Bosworth <gboswort@ucsc.edu>
2022-04-10 20:54:13 -0700

    debug

--- Rocket Patrol/src/Scenes/Play.js
+++ Rocket Patrol/src/Scenes/Play.js
@@ -16,7 +16,7 @@ class Play extends Phaser.Scene {
       this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
       this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
       this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-      this.Scene.start("playScene");
+      this.scene.start("playScene");
 
     }
   }
