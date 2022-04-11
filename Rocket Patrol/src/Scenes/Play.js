dd5fb96d8741a06d164ec9a4f855f43e5887c8b8
Grant Bosworth <gboswort@ucsc.edu>
2022-04-10 20:51:55 -0700

    e

--- Rocket Patrol/src/Scenes/Play.js
+++ Rocket Patrol/src/Scenes/Play.js
@@ -2,7 +2,12 @@ class Play extends Phaser.Scene {
     constructor() {
       super("playScene");
     }
-    
+    preload() {
+      // load images/tile sprites
+      this.load.image('rocket', './assets/rocket.png');
+      this.load.image('spaceship', './assets/spaceship.png');
+      this.load.image('starfield', './assets/starfield.png');
+    }
     create() {
       // green UI background
       this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
@@ -11,7 +16,7 @@ class Play extends Phaser.Scene {
       this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
       this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
       this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-      this.scene.start("playScene");
+      this.Scene.start("playScene");
 
     }
   }
